dba5267d5d5192aa39f71fd5bf2a7073df5f439e
Margaret Williford <margaretwilliford4@gmail.com>
2018-04-13 10:52:20 -0600

    user sees name address and length on trail show page

--- spec/features/trails/visitor_sees_trails_spec.rb
+++ spec/features/trails/visitor_sees_trails_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'As a user' do
   context 'when I visit trail show page' do
-    it 'I see list of names of all trips' do
+    it 'I see all trips' do
       trail = Trail.create(name: 'Mt Elbert', address: 'Colorado', length: 11)
       trip_1 = trail.trips.create(name: 'v fun adventure')
       trip_2 = trail.trips.create(name: 'v v fun adventure')
@@ -11,6 +11,15 @@ describe 'As a user' do
 
       expect(page).to have_content(trip_1.name)
       expect(page).to have_content(trip_2.name)
+    end
+    it 'I see everything about the hike' do
+      trail = Trail.create(name: 'Mt Elbert', address: 'Colorado', length: 11)
+
+      visit trail_path(trail)
+
+      expect(page).to have_content(trail.name)
+      expect(page).to have_content(trail.address)
+      expect(page).to have_content(trail.length)
     end
   end
 end
